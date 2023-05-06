78f39aa2ecb6158b6c0bc9b00e3a889c62b0dccb
Oksana Gluschenko <oksana_a_g@ukr.net>
2023-05-06 21:16:27 +0300

    fix

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { StyleSheet, View } from "react-native";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import LoginScreen from "./screens/LoginScreen";
 import RegistrationScreen from "./screens/RegistrationScreen";
 
- const [fonts] = useFonts({
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+   const [fonts] = useFonts({
     RobotoBold: require("../assets/fonts/Roboto-Bold.ttf"),
     RobotoRegular: require("../assets/fonts/Roboto-Regular.ttf"),
   });
@@ -17,16 +20,15 @@ import RegistrationScreen from "./screens/RegistrationScreen";
   }, [fonts]);
   if (!fonts) {
     return null;
-  }
+  };
 
-export default function App() {
   return (
     <View style={styles.container} onLayout={onLayoutRootView}>
       <RegistrationScreen />
       <LoginScreen />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
