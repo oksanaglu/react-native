5d61f328ae02d59df6b7ff9806d1c3c5032853e0
Oksana Gluschenko <oksana_a_g@ukr.net>
2023-05-06 21:17:36 +0300

    Update App.js

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ export default function App() {
 
   return (
     <View style={styles.container} onLayout={onLayoutRootView}>
-      <RegistrationScreen />
+      {/* <RegistrationScreen /> */}
       <LoginScreen />
     </View>
   );
